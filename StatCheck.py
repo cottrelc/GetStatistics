e21272ebec95c39d7491c497866c09e1a262d2a6
Carson <127548915+cottrelc@users.noreply.github.com>
2024-03-07 17:04:14 -0700

    Update StatCheck.py

--- StatCheck.py
+++ StatCheck.py
@@ -20,47 +20,50 @@ console.print(
 
 #TODO: Give choice to plus or minus number
 import math
-number = float(input("Enter number here: "))
-while True:
-    y = input("What would you like to do (+ or - or * or / or square or root): ")
+def main():
+    number = float(input("Enter number here: "))
+    while True:
+        y = input("What would you like to do (+ or - or * or / or square or root): ")
 
-    if y == "+":
-        add = float(input("Enter number: "))
-        number += add
-        console.print(f"[bold red]Result[/bold red]: {number}")
-    elif y == "-":
-        minus = float(input("Enter number: "))
-        number -= minus
-        console.print(f"[bold red]Result[/bold red]: {number}")
-    elif y == "*":
-        multiply = float(input("Enter number: "))
-        number *= multiply
-        console.print(f"[bold red]Result[/bold red]: {number}")
-    elif y == "/":
-        divide = float(input("Enter number: "))
-        number /= divide
-        console.print(f"[bold red]Result[/bold red]: {number}")
-    elif y == "square":
-        square = int(input("How many times would you like to times it by itself: "))
-        answer = number
-        for i in range(square - 1):
-            number= answer * number
-        console.print(f"[bold red]Result[/bold red]: {number}")
-    elif y =="root":
-        number = math.sqrt(number)
-        console.print(f"[bold red]Result[/bold red]: {number}")
-    else:
-        print("Please put in a sign.")
-    t = input("Would you like to do more math more (y/n): ")
-    if t == "n":
-        break
-    elif t == "y":
-        continue
-    else:
-        print("Please Enter y or n")
+        if y == "+":
+            add = float(input("Enter number: "))
+            number += add
+            console.print(f"[bold red]Result[/bold red]: {number}")
+        elif y == "-":
+            minus = float(input("Enter number: "))
+            number -= minus
+            console.print(f"[bold red]Result[/bold red]: {number}")
+        elif y == "*":
+            multiply = float(input("Enter number: "))
+            number *= multiply
+            console.print(f"[bold red]Result[/bold red]: {number}")
+        elif y == "/":
+            divide = float(input("Enter number: "))
+            number /= divide
+            console.print(f"[bold red]Result[/bold red]: {number}")
+        elif y == "square":
+            square = int(input("How many times would you like to times it by itself: "))
+            answer = number
+            for i in range(square - 1):
+                number= answer * number
+            console.print(f"[bold red]Result[/bold red]: {number}")
+        elif y =="root":
+            number = math.sqrt(number)
+            console.print(f"[bold red]Result[/bold red]: {number}")
+        else:
+            print("Please put in a sign.")
+        t = input("Would you like to do more math more (y/n): ")
+        if t == "n":
+            break
+        elif t == "y":
+            continue
+        else:
+            print("Please Enter y or n")
 
-console.print(f"[bold green]Answer[/bold green]: {number}")
+    console.print(f"[bold green]Answer[/bold green]: {number}")
 #More info
+    
+main()
 
 
     
